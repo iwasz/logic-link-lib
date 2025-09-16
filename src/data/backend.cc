d42d7af82d2e022d03da46938d9b356fe3ce632e
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2025-09-16 13:42:43 +0200

    Work on the backend and UTs for it.

--- src/data/backend.cc
+++ src/data/backend.cc
@@ -9,23 +9,38 @@
 module;
 #include "common/constants.hh"
 #include <algorithm>
-#include <span>
+#include <mutex>
+#include <ranges>
 #include <vector>
 module logic.data;
+import logic.core;
 
 namespace logic {
 
-void ChannelBlockStream::append (std::vector<Bytes> &&channels)
+ChannelBlock::ChannelBlock (size_t channels, size_t numberOfSampl) : data (channels)
 {
-        auto cb = ChannelBlock{currentSampleNo, std::move (channels)};
-        currentSampleNo = currentSampleNo + cb.size ();
-        data_.push_back (std::move (cb));
+        for (auto &ch : data) {
+                ch.resize (numberOfSampl);
+        }
 }
 
 /****************************************************************************/
 
+void ChannelBlockStream::append (std::vector<Bytes> &&channels)
+{
+        auto cb = ChannelBlock{currentSampleNo, std::move (channels)};
+        currentSampleNo = currentSampleNo + cb.channelSize ();
+        data_.push_back (std::move (cb));
+}
+
+/*--------------------------------------------------------------------------*/
+
 ChannelBlockStream::SubRange ChannelBlockStream::range (SampleIdx const &begin, SampleIdx const &end)
 {
+        if (begin == end) {
+                return {};
+        }
+
         auto b = std::find_if (data_.cbegin (), data_.cend (),
                                [&begin] (ChannelBlock const &b) { return b.firstSampleNo () <= begin && b.lastSampleNo () >= begin; });
 
@@ -41,12 +56,55 @@ ChannelBlockStream::SubRange ChannelBlockStream::range (SampleIdx const &begin, 
 
 /****************************************************************************/
 
-BlockBackend::SubRange BlockBackend::range (size_t groupIdx, SampleIdx const &begin, SampleIdx const &end)
+void BlockBackend::append (size_t groupIdx, std::vector<Bytes> &&s)
 {
-        return groups.at (groupIdx).range (begin, end);
+        std::lock_guard lock{mutex};
+        groups.at (groupIdx).append (std::move (s));
 }
 
-/****************************************************************************/
+/*--------------------------------------------------------------------------*/
+
+ChannelStream BlockBackend::range (size_t groupIdx, SampleIdx const &begin, SampleIdx const &end)
+{
+        std::lock_guard lock{mutex};
+        auto &group = groups.at (groupIdx);
+        auto subRangeBlocks = group.range (begin, end);
+
+        if (subRangeBlocks.empty ()) {
+                return {};
+        }
+
+        SampleNum totalLenToCopy = std::min<SampleNum> (end, subRangeBlocks.back ().lastSampleNo () + 1) - begin;
+        ChannelStream trimmedSubrange{group.channelsNumber (), size_t (totalLenToCopy)};
+
+        size_t cnt{};
+        SampleNum outputStartOffset{};
+
+        for (auto const &subRangeBlock : subRangeBlocks) {
+                size_t channelNo{};
+                // If first block, then offset != 0. For every other block offset == 0.
+                auto inputStartOffset = (cnt++ == 0) ? (begin - subRangeBlock.firstSampleNo ()) : (0);
+
+                auto inputLenToCopy = std::min<SampleNum> (totalLenToCopy, subRangeBlock.data.front ().size () - inputStartOffset);
+                totalLenToCopy -= inputLenToCopy;
+
+                for (auto const &channelInBlock : subRangeBlock.data) {
+                        auto begin = std::next (channelInBlock.cbegin (), inputStartOffset);
+                        auto end = std::next (begin, inputLenToCopy);
+                        std::copy (begin, end, std::next (trimmedSubrange.data.at (channelNo++).begin (), outputStartOffset));
+                }
+
+                outputStartOffset += inputLenToCopy;
+        }
+
+        if (totalLenToCopy != 0) {
+                throw Exception{"BlockBackend::range internal problem."};
+        }
+
+        return trimmedSubrange;
+}
+
+/*--------------------------------------------------------------------------*/
 
 void BlockBackend::configureGroup (size_t groupIdx, SampleRate sampleRate)
 {
@@ -55,7 +113,7 @@ void BlockBackend::configureGroup (size_t groupIdx, SampleRate sampleRate)
                 groups.resize (groupIdx + 1);
         }
 
-        groups.at (groupIdx).sampleRate = sampleRate;
+        groups.at (groupIdx).setSampleRate (sampleRate);
 }
 
 } // namespace logic
--- test/unit/backend.cc
+++ test/unit/backend.cc
@@ -13,12 +13,16 @@ import utils;
 
 using namespace logic;
 
+/**
+ * One block with 4 channels. 4 samples long.
+ */
 TEST_CASE ("one block", "[backend]")
 {
+        ChannelBlockStream cbs;
+        cbs.append (getChannelBlockData (0));
+
         SECTION ("0..4")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
 
                 auto r = cbs.range (0, 4);
                 REQUIRE (r.begin () == cbs.data ().begin ());
@@ -27,9 +31,6 @@ TEST_CASE ("one block", "[backend]")
 
         SECTION ("0..3")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-
                 auto r = cbs.range (0, 3);
                 REQUIRE (r.begin () == cbs.data ().begin ());
                 REQUIRE (r.end () == cbs.data ().end ());
@@ -37,9 +38,6 @@ TEST_CASE ("one block", "[backend]")
 
         SECTION ("0..2")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-
                 auto r = cbs.range (0, 2);
                 REQUIRE (r.begin () == cbs.data ().begin ());
                 REQUIRE (r.end () == cbs.data ().end ());
@@ -47,32 +45,30 @@ TEST_CASE ("one block", "[backend]")
 
         SECTION ("0..0")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-
+                // Empty range should result in 0 blocks.
                 auto r = cbs.range (0, 0);
-                REQUIRE (r.begin () == cbs.data ().begin ());
-                REQUIRE (r.end () == cbs.data ().end ());
+                REQUIRE (r.size () == 0);
         }
 
         SECTION ("4..8")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-
                 auto r = cbs.range (4, 8);
                 REQUIRE (std::empty (r));
         }
 }
 
+/**
+ * Three blocks with 4 channels. Each is 4 samples long -> stream is 12 samples long.
+ */
 TEST_CASE ("many blocks", "[backend]")
 {
+        ChannelBlockStream cbs;
+        cbs.append (getChannelBlockData (0));
+        cbs.append (getChannelBlockData (1));
+        cbs.append (getChannelBlockData (2));
+
         SECTION ("1 block")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-                cbs.append (getChannelBlockData (1));
-                cbs.append (getChannelBlockData (2));
 
                 auto r = cbs.range (0, 3);
                 REQUIRE (r.begin () == cbs.data ().begin ());
@@ -81,11 +77,6 @@ TEST_CASE ("many blocks", "[backend]")
 
         SECTION ("2 blocks")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-                cbs.append (getChannelBlockData (1));
-                cbs.append (getChannelBlockData (2));
-
                 auto r = cbs.range (0, 4);
                 REQUIRE (r.begin () == cbs.data ().begin ());
                 REQUIRE (r.end () == std::next (cbs.data ().begin (), 2));
@@ -97,11 +88,6 @@ TEST_CASE ("many blocks", "[backend]")
 
         SECTION ("all blocks")
         {
-                ChannelBlockStream cbs;
-                cbs.append (getChannelBlockData (0));
-                cbs.append (getChannelBlockData (1));
-                cbs.append (getChannelBlockData (2));
-
                 auto r = cbs.range (0, 8);
                 REQUIRE (r.begin () == cbs.data ().begin ());
                 REQUIRE (r.end () == cbs.data ().end ());
@@ -114,14 +100,135 @@ TEST_CASE ("many blocks", "[backend]")
                 REQUIRE (r.begin () == cbs.data ().begin ());
                 REQUIRE (r.end () == cbs.data ().end ());
         }
+
+        SECTION ("last block")
+        {
+                auto r = cbs.range (8, 100);
+                REQUIRE (r.begin () == std::next (cbs.data ().begin (), 2));
+                REQUIRE (r.end () == cbs.data ().end ());
+        }
 }
 
+/**
+ *
+ */
 TEST_CASE ("data", "[backend]")
 {
         BlockBackend backend;
         backend.configureGroup (0, 240'000'000);
         backend.append (0, getChannelBlockData (0));
+        backend.append (0, getChannelBlockData (1));
+        backend.append (0, getChannelBlockData (2));
 
-        // auto chBlockSpan = backend.range (0, 0, 4);
-        // REQUIRE (chBlockSpan.size () == 1);
+        SECTION ("empty range")
+        {
+                auto stream = backend.range (0, 0, 0);
+                REQUIRE (stream.channelSize () == 0);
+        }
+
+        SECTION ("first single byte")
+        {
+                auto stream = backend.range (0, 0, 1);
+                REQUIRE (stream.channelSize () == 1);
+                auto const &ch0 = stream.data.at (0);
+                REQUIRE (ch0.size () == 1);
+                REQUIRE (ch0.front () == 0x0);
+
+                auto const &ch1 = stream.data.at (1);
+                REQUIRE (ch1.front () == 0x1);
+
+                auto const &ch2 = stream.data.at (2);
+                REQUIRE (ch2.front () == 0x2);
+
+                auto const &ch3 = stream.data.at (3);
+                REQUIRE (ch3.front () == 0x3);
+        }
+
+        SECTION ("second single byte")
+        {
+                auto stream = backend.range (0, 1, 2);
+                REQUIRE (stream.channelSize () == 1);
+                REQUIRE (stream.data == std::vector<Bytes>{{0x1}, {0x2}, {0x3}, {0x4}});
+        }
+
+        SECTION ("single byte from second block")
+        {
+                auto stream = backend.range (0, 5, 6);
+                REQUIRE (stream.channelSize () == 1);
+                REQUIRE (stream.data == std::vector<Bytes>{{0x5}, {0x6}, {0x7}, {0x8}});
+        }
+
+        SECTION ("last single byte")
+        {
+                auto stream = backend.range (0, 11, 12);
+                REQUIRE (stream.channelSize () == 1);
+                REQUIRE (stream.data == std::vector<Bytes>{{0xb}, {0xc}, {0xd}, {0xe}});
+        }
+
+        SECTION ("outside range")
+        {
+                auto stream = backend.range (0, 12, 13);
+                REQUIRE (stream.channelSize () == 0);
+        }
+
+        SECTION ("two bytes at the front")
+        {
+                auto stream = backend.range (0, 0, 2);
+                REQUIRE (stream.channelSize () == 2);
+                REQUIRE (stream.data == std::vector<Bytes>{{0x0, 0x1}, {0x1, 0x2}, {0x2, 0x3}, {0x3, 0x4}});
+        }
+
+        SECTION ("two bytes spanning two blocks")
+        {
+                auto stream = backend.range (0, 3, 5);
+                REQUIRE (stream.channelSize () == 2);
+                REQUIRE (stream.data == std::vector<Bytes>{{0x3, 0x4}, {0x4, 0x5}, {0x5, 0x6}, {0x6, 0x7}});
+        }
+
+        SECTION ("last two bytes")
+        {
+                auto stream = backend.range (0, 10, 12);
+                REQUIRE (stream.channelSize () == 2);
+                REQUIRE (stream.data == std::vector<Bytes>{{0xa, 0xb}, {0xb, 0xc}, {0xc, 0xd}, {0xd, 0xe}});
+        }
+
+        SECTION ("two bytes requested, only one returned from the back")
+        {
+                auto stream = backend.range (0, 11, 13);
+                REQUIRE (stream.channelSize () == 1);
+                REQUIRE (stream.data == std::vector<Bytes>{{0xb}, {0xc}, {0xd}, {0xe}});
+        }
+
+        SECTION ("more than a block")
+        {
+                auto stream = backend.range (0, 0, 6);
+                REQUIRE (stream.channelSize () == 6);
+                REQUIRE (stream.data
+                         == std::vector<Bytes>{{0x0, 0x1, 0x2, 0x3, 0x4, 0x5},
+                                               {0x1, 0x2, 0x3, 0x4, 0x5, 0x6},
+                                               {0x2, 0x3, 0x4, 0x5, 0x6, 0x7},
+                                               {0x3, 0x4, 0x5, 0x6, 0x7, 0x8}});
+        }
+
+        SECTION ("more than a block")
+        {
+                auto stream = backend.range (0, 6, 12);
+                REQUIRE (stream.channelSize () == 6);
+                REQUIRE (stream.data
+                         == std::vector<Bytes>{{0x6, 0x7, 0x8, 0x9, 0xa, 0xb},
+                                               {0x7, 0x8, 0x9, 0xa, 0xb, 0xc},
+                                               {0x8, 0x9, 0xa, 0xb, 0xc, 0xd},
+                                               {0x9, 0xa, 0xb, 0xc, 0xd, 0xe}});
+        }
+
+        SECTION ("full range")
+        {
+                auto stream = backend.range (0, 0, 100);
+                REQUIRE (stream.channelSize () == 12);
+                REQUIRE (stream.data
+                         == std::vector<Bytes>{{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb},
+                                               {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc},
+                                               {0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd},
+                                               {0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe}});
+        }
 }
