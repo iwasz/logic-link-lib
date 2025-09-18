8f734d1b39a830af2a08569e9f71bc22c13ef6bc
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2025-09-18 15:05:12 +0200

    Proper bitrate (speed) generated in the DemoDevice.

--- src/peripheral/demo/demoDevice.cc
+++ src/peripheral/demo/demoDevice.cc
@@ -9,6 +9,7 @@
 module;
 #include "common/constants.hh"
 #include "common/params.hh"
+#include <spdlog/spdlog.h>
 #include <thread>
 module logic.peripheral;
 import logic.processing;
@@ -28,8 +29,12 @@ DemoDevice::DemoDevice (EventQueue *eventQueue) : eventQueue_{eventQueue}
 void DemoDevice::start (IBackend *backend)
 {
         notify (true, State::ok);
+        totalSizePerChan = 0;
 
         thread = std::thread{[backend, this] {
+                double delay = double (transferSize) * CHAR_BIT / acquisitionParams.digitalSampleRate;
+                auto chDelay = std::chrono::round<std::chrono::microseconds> (std::chrono::duration<double> (delay));
+
                 while (running ()) {
                         auto now = std::chrono::steady_clock::now ();
 
@@ -37,20 +42,22 @@ void DemoDevice::start (IBackend *backend)
                         std::vector<Bytes> channels;
                         channels.reserve (dc);
 
+                        auto sizePerChan = transferSize * CHAR_BIT / dc;
+                        totalSizePerChan += sizePerChan;
+
                         for (auto i = 0U; i < dc; ++i) {
                                 /*
                                  * Square wave which frequency increases proportionally with the
                                  * channel number and length (number of bits) decreases.
                                  */
-                                channels.push_back (generators.at (i) (i + 1, i + 1, transferSize * CHAR_BIT / dc));
+                                channels.push_back (generators.at (i) (i + 1, i + 1, sizePerChan));
                         }
 
                         static constexpr auto BITS_PER_SAMPLE = 1U;
                         static constexpr auto GROUP = 0U;
                         backend->append (GROUP, BITS_PER_SAMPLE, std::move (channels));
 
-                        auto timeSpent = std::chrono::seconds (transferSize / acquisitionParams.digitalSampleRate);
-                        std::this_thread::sleep_until (now + timeSpent);
+                        std::this_thread::sleep_until (now + chDelay);
                 }
         }};
 }
@@ -60,7 +67,9 @@ void DemoDevice::start (IBackend *backend)
 void DemoDevice::stop ()
 {
         try {
+                spdlog::info ("DemoDevice stopped. Samples generated per channel: {}", totalSizePerChan);
                 notify (false, State::ok);
+
                 if (thread.joinable ()) {
                         thread.join ();
                 }
